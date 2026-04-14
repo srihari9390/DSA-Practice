da5f173d046dde080063bf2265633df880a849db
srihari9390 <145956954+srihari9390@users.noreply.github.com>
2026-04-14 15:20:15 +0530

    Update SumOfTheArray.java

--- LeetCode/Arrays/OneDimentnalArrays/SumOfTheArray.java
+++ LeetCode/Arrays/OneDimentnalArrays/SumOfTheArray.java
@@ -1,1 +1,8 @@
 LeetCode/Arrays/OneDimentnalArrays/
+Class SumOfTheArray
+{
+  Public Static Void Main(String Args[])
+    {
+    System.out.println("Hello Srihari this is your first Program");
+    }
+}
